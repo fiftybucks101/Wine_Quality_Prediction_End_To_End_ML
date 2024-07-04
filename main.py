72344daf4267f74a1b8028d3c872911e9db4aa0a
Fifty Bucks <fiftybucks001@gmail.com>
2024-07-04 10:40:59 +0545

    Modular Data Ingestion Added

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-from src.wine_pred.logging.logger import logger
+from wine_pred.config.configuration import ConfigurationManager
+from wine_pred.logging.logger import logger
+from wine_pred.components.data_ingestion import DataIngestion
 
-logger.info('Hello logging check!')
+STAGE_NAME = "Data Ingestion stage"
+try:
+    config = ConfigurationManager()
+    data_ingestion_config = config.get_data_ingestion_config()
+    data_ingestion = DataIngestion(config=data_ingestion_config)
+    data_ingestion.download_file()
+    data_ingestion.extract_zip_file()
+except Exception as e:
+    logger.exception(e)
+    raise e
